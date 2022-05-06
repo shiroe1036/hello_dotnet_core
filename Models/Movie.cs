8364f257609c8adf0b7b079f05f886b908c0ca4c
Eric RANDRETSARIMANANA <eric_ra@absys.fr>
2022-05-06 16:17:48 +0200

    finish new field

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -31,7 +31,8 @@ namespace RazorPagesMovie.Models
                 Title = "When Harry Met Sally",
                 ReleaseDate = DateTime.Parse("1989-2-12"),
                 Genre = "Romantic Comedy",
-                Price = 7.99M
+                Price = 7.99M,
+                Rating = "R"
             },
 
             new Movie
@@ -39,7 +40,8 @@ namespace RazorPagesMovie.Models
                 Title = "Ghostbusters ",
                 ReleaseDate = DateTime.Parse("1984-3-13"),
                 Genre = "Comedy",
-                Price = 8.99M
+                Price = 8.99M,
+                Rating = "R"
             },
 
             new Movie
@@ -47,7 +49,8 @@ namespace RazorPagesMovie.Models
                 Title = "Ghostbusters 2",
                 ReleaseDate = DateTime.Parse("1986-2-23"),
                 Genre = "Comedy",
-                Price = 9.99M
+                Price = 9.99M,
+                Rating = "R"
             },
 
             new Movie
@@ -55,7 +58,8 @@ namespace RazorPagesMovie.Models
                 Title = "Rio Bravo",
                 ReleaseDate = DateTime.Parse("1959-4-15"),
                 Genre = "Western",
-                Price = 3.99M
+                Price = 3.99M,
+                Rating = "R"
             }
                 );
             context.SaveChanges();
@@ -75,5 +79,6 @@ namespace RazorPagesMovie.Models
         
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+        public string Rating { get; set; } = string.Empty;
     }
 }
