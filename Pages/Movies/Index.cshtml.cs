6389974164ea4588da501be329652128e294a017
Eric RANDRETSARIMANANA <eric_ra@absys.fr>
2022-05-06 13:59:13 +0200

    MAJ dotnet app

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,46 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace netapp.Pages_Movies
+{
+#pragma warning disable CS8618
+#pragma warning disable CS8604
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var movies = from m in _context.Movie select m;
+
+            if(!string.IsNullOrEmpty(SearchString)){
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync();
+        }
+    }
+#pragma warning restore CS8618
+#pragma warning restore CS8604
+}
